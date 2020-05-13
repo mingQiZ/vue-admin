b3c0925ac4c6e00cee65e9141fa019ae321bd04f
mingQiZ <2052176762@qq.com>
2020-05-13 22:10:19 +0800

    构建项目、配置全局样式、路由、element

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login/index")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    pieiiiiieiie这是登陆页
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
